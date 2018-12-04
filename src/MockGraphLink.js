a907ddf0a27c4ee1e0a1e1f184ad0206d629cdd2
Dallon Feldner <dfeldner@phunware.com>
2018-12-04 12:46:45 -0600

    Refactor logic to a copy of graphql-anywhere

--- src/MockGraphLink.js
+++ src/MockGraphLink.js
@@ -1,5 +1,28 @@
 import { ApolloLink, Observable } from 'apollo-link';
-import graphqlAnywhere from 'graphql-anywhere';
+import {
+  getMainDefinition,
+  getFragmentDefinitions,
+  createFragmentMap,
+  shouldInclude,
+  isField,
+  resultKeyNameFromField,
+  isInlineFragment,
+  argumentsObjectFromField,
+} from 'apollo-utilities';
+
+/// copied from graphql-anywhere
+export function merge(dest, src) {
+  if (src !== null && typeof src === 'object') {
+    Object.keys(src).forEach(key => {
+      const srcVal = src[key];
+      if (!Object.prototype.hasOwnProperty.call(dest, key)) {
+        dest[key] = srcVal;
+      } else {
+        merge(dest[key], srcVal);
+      }
+    });
+  }
+}
 
 class MockGraphLink extends ApolloLink {
   constructor(getMockGraph) {
@@ -36,49 +59,143 @@ class MockGraphLink extends ApolloLink {
       );
     }
 
-    const result = graphqlAnywhere(
-      (fieldName, rootValue, args) => {
-        const mockedValue = rootValue[fieldName];
-        if (mockedValue === undefined) {
-          throw new Error(
-            `Value for field ${fieldName} is missing in query ${
-              operation.operationName
-            }`
-          );
-        } else if (typeof mockedValue === 'function') {
-          const resolvedValue = mockedValue(args || {});
-          if (resolvedValue === undefined) {
-            throw new Error(
-              `Resolver for field ${fieldName} in query ${
-                operation.operationName
-              } returned undefined for arguments ${JSON.stringify(
-                args
-              )}. If null is intended, return it explicitly.`
-            );
-          } else {
-            return resolvedValue;
+    // Based on the source of graphql-anywhere
+    const mainDefinition = getMainDefinition(operation.query);
+    const fragments = getFragmentDefinitions(operation.query);
+    const fragmentMap = createFragmentMap(fragments);
+
+    const executeSelectionSet = (selectionSet, rootValue) => {
+      const result = {};
+      selectionSet.selections.forEach(selection => {
+        if (!shouldInclude(selectionSet, operation.variables)) {
+          // Skip this entirely
+          return;
+        }
+
+        if (isField(selection)) {
+          const fieldResult = executeField(selection, rootValue);
+
+          const resultFieldKey = resultKeyNameFromField(selection);
+
+          if (fieldResult !== undefined) {
+            if (result[resultFieldKey] === undefined) {
+              result[resultFieldKey] = fieldResult;
+            } else {
+              merge(result[resultFieldKey], fieldResult);
+            }
           }
-        } else if (
-          args &&
-          Object.keys(args).length &&
-          typeof mockedValue !== 'function'
-        ) {
+        } else {
+          let fragment;
+
+          if (isInlineFragment(selection)) {
+            fragment = selection;
+          } else {
+            // This is a named fragment
+            fragment = fragmentMap[selection.name.value];
+
+            if (!fragment) {
+              throw new Error(`No fragment named ${selection.name.value}`);
+            }
+          }
+
+          const fragmentResult = executeSelectionSet(
+            fragment.selectionSet,
+            rootValue
+          );
+
+          merge(result, fragmentResult);
+        }
+      });
+
+      return result;
+    };
+
+    const executeField = (field, rootValue) => {
+      const fieldName = field.name.value;
+      const args = argumentsObjectFromField(field, operation.variables);
+
+      const mockedValue = rootValue[fieldName];
+      let result;
+      if (mockedValue === undefined) {
+        throw new Error(
+          `Value for field ${fieldName} is missing in query ${
+            operation.operationName
+          }`
+        );
+      }
+      if (mockedValue === undefined) {
+        throw new Error(
+          `Value for field ${fieldName} is missing in query ${
+            operation.operationName
+          }`
+        );
+      } else if (typeof mockedValue === 'function') {
+        const resolvedValue = mockedValue(args || {});
+        if (resolvedValue === undefined) {
           throw new Error(
-            `Field ${fieldName} in query ${
+            `Resolver for field ${fieldName} in query ${
               operation.operationName
-            } takes arguments (${JSON.stringify(
+            } returned undefined for arguments ${JSON.stringify(
               args
-            )}), so it must be mocked as a function`
+            )}. If null is intended, return it explicitly.`
           );
         } else {
-          return mockedValue;
+          result = resolvedValue;
         }
-      },
-      operation.query,
-      rootValue,
-      {},
-      operation.variables
-    );
+      } else if (
+        args &&
+        Object.keys(args).length &&
+        typeof mockedValue !== 'function'
+      ) {
+        throw new Error(
+          `Field ${fieldName} in query ${
+            operation.operationName
+          } takes arguments (${JSON.stringify(
+            args
+          )}), so it must be mocked as a function`
+        );
+      } else {
+        result = mockedValue;
+      }
+
+      // Handle all scalar types here
+      if (!field.selectionSet) {
+        return result;
+      }
+
+      // From here down, the field has a selection set, which means it's trying to
+      // query a GraphQLObjectType
+      if (result === null) {
+        // Basically any field in a GraphQL response can be null, or missing
+        return result;
+      }
+
+      if (Array.isArray(result)) {
+        return executeSubSelectedArray(field, result);
+      }
+
+      // Returned value is an object, and the query has a sub-selection. Recurse.
+      return executeSelectionSet(field.selectionSet, result);
+    };
+
+    const executeSubSelectedArray = (field, result) => {
+      return result.map(item => {
+        // null value in array
+        if (item === null) {
+          return null;
+        }
+
+        // This is a nested array, recurse
+        if (Array.isArray(item)) {
+          return executeSubSelectedArray(field, item);
+        }
+
+        // This is an object, run the selection set on it
+        return executeSelectionSet(field.selectionSet, item);
+      });
+    };
+
+    const result = executeSelectionSet(mainDefinition.selectionSet, rootValue);
 
     return new Observable(sub => {
       const timeout = setTimeout(() => {
