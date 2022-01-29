b03c0a8427f06e547895398229b4386addd4a8ec
Dallon Feldner <dallon.feldner@gmail.com>
2022-01-29 11:39:52 -0600

    Upgrade Neutrino

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,3 @@
-// .eslintrc.js
-const { Neutrino } = require('neutrino');
+const neutrino = require("neutrino");
 
-// Specify middleware to Neutrino prior to calling eslintrc.
-// Even if using .neutrinorc.js, you must specify it when using
-// the API
-module.exports = Neutrino({ root: __dirname })
-  .use('@neutrinojs/eslint')
-  .use('.neutrinorc.js')
-  .call('eslintrc');
+module.exports = neutrino().eslintrc();
